7c7a7c3f56162518f4966cee68d3aebf5b8a7e7f
Paulo Orquillo <paulo.orquillo@davanti.co.nz>
2020-03-22 00:42:28 +1300

    adding animations

--- Lynda-Huli Pizza/PageTitleView.swift
+++ Lynda-Huli Pizza/PageTitleView.swift
@@ -20,7 +20,10 @@ struct PageTitleView: View {
                 .padding(.trailing)
             
         }.overlay(
-            Image(systemName:isDisplayingOrders ?? false ? "chevron.up.square" : "chevron.down.square")
+            Image(systemName:"chevron.up.square")
+            .rotationEffect(isDisplayingOrders ?? false ?
+                Angle(degrees: 0.0) : Angle(degrees: 180.0))
+                .animation(.easeInOut(duration: 0.5))
                 .font(.title)
                 .foregroundColor(isDisplayingOrders != nil ? Color("G1"):.clear)            
                 .padding(),
@@ -33,6 +36,6 @@ struct PageTitleView: View {
 
 struct PageTitleView_Previews: PreviewProvider {
     static var previews: some View {
-        PageTitleView(title:"Order Pizza")
+        PageTitleView(title:"Order Pizza", isDisplayingOrders: true)
     }
 }
